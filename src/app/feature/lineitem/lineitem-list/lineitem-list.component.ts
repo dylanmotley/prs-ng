0fb0ac7e642daf673955b69c63e30d225e19e80e
Dylan Motley <motley.dylan60@gmail.com>
2021-08-15 23:25:32 -0400

    Line Item

--- src/app/feature/lineitem/lineitem-list/lineitem-list.component.ts
+++ src/app/feature/lineitem/lineitem-list/lineitem-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineitemListComponent implements OnInit {
 
+  // LineItem Constructor
   constructor() { }
 
   ngOnInit(): void {
